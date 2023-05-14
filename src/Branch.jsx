5174fbb27e687052b9727b4794531d7f76449721
Connor <connor.cs@protonmail.com>
2023-05-14 16:24:24 -0400

    move branches

--- src/Branch.jsx
+++ src/Branch.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 
-export default function Branch({name}) {
-    console.log({name})
+export default function Branch({ name, handleMoveColumn }) {
+  
   return (
-    <div className='branch'>
-        {name}
-        <h1>alsdkjf;alsdkjf;lasdkj</h1>
+    <div className="branch" key={name}>
+      <MdKeyboardArrowLeft className="button" onClick={() => handleMoveColumn("left", name)} />
+      {name}
+      <MdKeyboardArrowRight className="button"onClick={() => handleMoveColumn("right", name)} />
     </div>
-  )
+  );
 }
--- src/BranchesPage.jsx
+++ src/BranchesPage.jsx
@@ -2,36 +2,45 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getRepo, getBranches } from "./api";
 import { AiOutlineArrowLeft, AiOutlineStar } from "react-icons/ai";
+import { MdKeyboardArrowRight } from "react-icons/md";
 import Branch from "./Branch";
+
+//how to organize data into changeable columns?
+//by default, items have index of 0
+//increment when moving rightward
 
 export default function BranchesPage() {
   const params = useParams();
-  const nav = useNavigate()
+  const nav = useNavigate();
   // console.log({ params });
   const [repoData, setRepoData] = useState({
     name: "",
     owner: "",
     desc: "",
     watchers: "",
-    branches: []
+    branches: [],
   });
+  const [branches, setBranches] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const br = await getBranches(params.owner, params.name)
-      const repo = await getRepo(params.owner, params.name)
+      const br = await getBranches(params.owner, params.name);
+      const repo = await getRepo(params.owner, params.name);
+
+      br.map((branch) => (branch["index"] = 0));
 
       setRepoData({
         ...repoData,
         name: repo.name,
         desc: repo.description,
         watchers: repo.watchers,
-        branches: br
-      })
-    }
+        branches: br,
+      });
 
-    getData()
+      setBranches(br);
+    };
 
+    getData();
   }, []);
 
   // useEffect(() => {
@@ -50,13 +59,14 @@ export default function BranchesPage() {
   //     );
   // }, []);
 
-  console.log({ repoData });
-  console.log(repoData.branches)
+  // console.log({ repoData });
+  // console.log(repoData.branches);
+  console.log({branches})
 
   return (
     <div className="main">
       <div className="header">
-        <button onClick={() => nav('/')}>
+        <button className="button" onClick={() => nav("/")}>
           <AiOutlineArrowLeft />
         </button>
         <div className="description">
@@ -70,20 +80,50 @@ export default function BranchesPage() {
       </div>
       <div className="branches-container column">
         <div className="in-progress column">
-          <p>In progress</p>
+          <p>In progress </p>
           <div className="in-progress-container">
-                        
-            {repoData.branches.length ? repoData.branches.map((_, i) => <p>{i}</p>) : null}
+            {repoData.branches.length
+              ? repoData.branches
+                  .filter((branch) => branch.index === 0)
+                  .map((branch, i) => (
+                    <Branch
+                      name={branch.name}
+                      handleMoveColumn={handleMoveColumn}
+                    />
+                  ))
+              : null}
           </div>
         </div>
         <div className="review column">
           Review
+          {branches
+            .filter((branch) => branch.index === 1)
+            .map((branch) => (
+              <Branch name={branch.name} handleMoveColumn={handleMoveColumn} />
+            ))}
         </div>
         <div className="merge column">
           Ready to Merge
+          {branches
+            .filter((branch) => branch.index === 2)
+            .map((branch) => (
+              <Branch name={branch.name} handleMoveColumn={handleMoveColumn} />
+            ))}
         </div>
       </div>
     </div>
   );
+
+  function handleMoveColumn(direction, name) {
+    console.log({branches})
+    //get all branches except the one being moved
+    const filteredBranches = [...branches].filter(branch=>branch.name!=name)
+    const movedBranch = [...branches].find((branch) => branch.name === name);
+    
+    if (direction == "left" && movedBranch.index>=1) movedBranch.index -= 1;
+    else if (direction == "right" && movedBranch.index<=1) movedBranch.index += 1;
+    
+    setBranches([filteredBranches, movedBranch].flat());
+  }
 
 }
